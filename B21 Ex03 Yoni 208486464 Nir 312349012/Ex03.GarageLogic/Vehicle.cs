2d801c3c3ae9d4c94c79f83bbc08216737c86067
yoni.m <yoni668@gmail.com>
2021-05-14 16:48:43 +0300

    no message

---
+++ B21 Ex03 Yoni 208486464 Nir 312349012/Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Vehicle
+    {
+		private readonly int r_NumOfWheels = 0;
+
+		private string m_ManufacturName;
+		private string m_PlateLicenseNumber;
+		private float m_RemainingEnergy;
+		private List<Wheel> m_Wheels;
+		//private EnergySource m_EnergySource;
+
+		public string ManufacturName
+        {
+            get { return m_ManufacturName; }
+			set { m_ManufacturName = value; }
+		}
+
+		public string PlateLicenseNumber
+        { 
+			get { return m_PlateLicenseNumber; }
+			set { m_PlateLicenseNumber = value; }
+		}
+
+		public float RemainingEnergy
+        {
+            get { return m_RemainingEnergy; }
+			set { m_RemainingEnergy = value; }
+		}
+		
+		public List<Wheel> Wheels
+        {
+			get { return m_Wheels; }
+			set { m_Wheels = value; }
+		}
+
+
+
+
+	}
+}
--- B21 Ex03 Yoni 208486464 Nir 312349012/Ex03.GarageLogic/Wheel.cs
+++ B21 Ex03 Yoni 208486464 Nir 312349012/Ex03.GarageLogic/Wheel.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    class Wheel
+    public class Wheel
     {
+        private string m_ManufacturName;
+        private float m_AirPressure;
+        private readonly float r_MaxAirPressure;
+
+
+        protected Wheel(string i_ManufacturName, float i_AirPressure, float i_MaxAirPressure)
+        {
+            m_ManufacturName = i_ManufacturName;
+            m_AirPressure = i_AirPressure;
+            r_MaxAirPressure = i_MaxAirPressure;
+        }
+
+
+        public string ManufactorNamer
+        {
+            get { return m_ManufacturName; }
+            set { m_ManufacturName = value; }
+        }
+
+        public float AirPressure
+        {
+            get { return m_AirPressure; }
+            set { m_AirPressure = value; }
+        }
+
+        public float MaxAirPressure
+        {
+            get { return r_MaxAirPressure; }
+        }
+
+
     }
 }
