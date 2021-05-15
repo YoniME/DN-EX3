a7d5748cf8225cc5e73423e325bc3ea1740232f1
yoni.m <yoni668@gmail.com>
2021-05-15 20:27:38 +0300

    no message

---
+++ B21 Ex03 Yoni 208486464 Nir 312349012/Ex03.ConsoleUI/UI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    public class UI
+    {
+
+        public enum eGarageActions
+        {
+            InsertNewVehicle = 1,
+            DisplayAllVehiclesInGarage,
+            ChangeTheStatusOfCar,
+            InflateTheWheels,
+            Refuel,
+            ChargeTheBattery,
+            DisplayFullDetailsOfVehicle,
+            Quit
+        }
+
+        public void PrintString(string i_StringToPrint)
+        {
+            Console.WriteLine(i_StringToPrint);
+        }
+
+        public string GetString()
+        {
+            return Console.ReadLine();
+        }
+
+        public eGarageActions GetActionFromUser()
+        {
+            eGarageActions action = new eGarageActions();
+            int userChoice = GetInputAccordingToEnum(action);
+
+
+
+            return (eGarageActions)userChoice;
+
+        }
+
+        public int GetInputAccordingToEnum<T>(T i_Enum)
+        {
+            string[] menu = Enum.GetNames(typeof(T));
+            Array indexes = Enum.GetValues(typeof(T));
+            int index = 0;
+            foreach(string str in menu)
+            {
+                Console.WriteLine(string.Format("{0}. {1}",str));
+            }
+
+
+        }
+
+    }
+}
