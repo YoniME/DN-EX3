eb2020b3286c9b9d618406fae486c2ac3d431b5a
nirweiler <nir.weilerf@gmail.com>
2021-05-20 19:36:13 +0300

    s

--- B21 Ex03 Yoni 208486464 Nir 312349012/Ex03.GarageLogic/Car.cs
+++ B21 Ex03 Yoni 208486464 Nir 312349012/Ex03.GarageLogic/Car.cs
@@ -69,7 +69,6 @@ namespace Ex03.GarageLogic
         {
             StringBuilder carDetails = new StringBuilder();
 
-            carDetails.AppendFormat("Vehicle type: {0}{1}", this.GetType().Name, Environment.NewLine);
             carDetails.AppendFormat("{0}", base.GetDetails());
             carDetails.AppendFormat("Color: {0}{1}", m_CarColor.ToString(), Environment.NewLine);
             carDetails.AppendFormat("Number of doors: {0}{1}", m_NumberOfDoors.ToString(), Environment.NewLine);
